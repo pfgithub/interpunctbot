e83937755d75fc7e6dff6e3bb2c3befb05bf7d83
pfg <omegariisthebest@gmail.com>
2018-05-16 21:01:37 -0700

    Removed and changed password

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
       "host": "127.0.0.1",
       "port": "5433",
       "user": "interpunctbot",
-      "password": "dbps*u(q)++[]",
+      "password": "6b 57 ea 58 20 2d ec e6 35 57 81 f2 b0 67 da 4d 0e 7e c5 44 d1 4c 0d f0",
       "database": "interpunctbot"
     }
   }
