ab55c55326825f9ba28c4d6bac9b0398c64c0a4e
pfg <omegariisthebest@gmail.com>
2018-05-21 07:15:49 -0700

    Handle unhandled promise rejection

--- src/commands/channelmanagement.js
+++ src/commands/channelmanagement.js
@@ -1,9 +1,9 @@
 const commands = new (require("../Commands"));
 const o = require("../options");
 
-function spaceChannels({guild, from, to}) {
+function spaceChannels({guild, from, to, msg}) {
   let channelNames = [];
-  guild.channels.forEach(channel => {if(channel.name.indexOf(from) > -1) { channelNames.push(`<#${channel.id}>`); channel.setName(channel.name.split(from).join(to)); }   });
+  guild.channels.forEach(channel => {if(channel.name.indexOf(from) > -1) { channelNames.push(`<#${channel.id}>`); channel.setName(channel.name.split(from).join(to)).catch(e => msg.reply(`Could not space channels because I don't have permission to manage <#${channel.id}>.`)); }   });
   return channelNames.length > 10 ? `${channelNames.length} channels` : `${  channelNames.join(", ")  }.`;
 }
 
@@ -13,11 +13,11 @@ commands.registerCommand("spaceChannels", [o.pm(false), o.myPerm("MANAGE_CHANNEL
     case "true":
     case "space":
     case "yes":
-      return data.msg.reply(`Spaced ${spaceChannels({"guild": data.msg.guild, "from": `-`, "to": ` `})}`);
+      return data.msg.reply(`Spaced ${spaceChannels({"guild": data.msg.guild, "from": `-`, "to": ` `, "msg": data.msg})}`);
     case "false":
     case "dash":
     case "no":
-      return data.msg.reply(`Dashed ${spaceChannels({"guild": data.msg.guild, "to": `-`, "from": ` `})}`);
+      return data.msg.reply(`Dashed ${spaceChannels({"guild": data.msg.guild, "to": `-`, "from": ` `, "msg": data.msg})}`);
     default:
       return data.msg.reply("Usage: spaceChannels [space|dash]");
   }
