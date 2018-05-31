c43aceb3bd57e0bc85ba14066d4013a7eeeb42c6
pfg <omegariisthebest@gmail.com>
2018-05-30 21:01:04 -0700

    Update leaderboard display

--- index.js
+++ index.js
@@ -119,20 +119,38 @@ usage.path("speedrun").add("leaderboard", new Usage({ // TODO trophy-1st for the
     }else{
       category = defaultCategory;
     }
-    let gameData = await sr.leaderboards(gameID, category).embed(["category", "players"]).exec();
-    let topFive = gameData.items.runs.filter(run => run.place<=5);
-    console.log(gameData.items.category.data.name);
+    let gameData = await sr.leaderboards(gameID, category).embed(["category", "players", "game"]).exec();
+    let actualGameData = gameData.items.game.data;
+    let topThree = gameData.items.runs.filter(run => run.place<=3);
     let getPlayer = player => gameData.items.players.data.filter(pl => pl.id === player)[0];
 
-    let resEmbed = new RichEmbed;
-    resEmbed.title = gameData.items.category.data.name;
-    resEmbed.description = gameData.items.category.data.weblink;
-    topFive.forEach(run => {
-      run = run.run;
+    let resEmbeds = [];
+    let mainEmbed = new RichEmbed;
+    mainEmbed.title = gameData.items.category.data.name;
+    mainEmbed.description = gameData.items.category.data.rules;
+    mainEmbed.url = gameData.items.category.data.weblink;
+    topThree.forEach(run_ => {
+      let run = run_.run;
+      let embed = new RichEmbed;
+      embed.title = `:${  run.times.primary_t}`;
+      embed.description = run.comment;
+      embed.url = run.weblink;
+      // embed.color = "random";
       let runPlayer = getPlayer(run.players[0].id);
-      resEmbed.addField(run.times.primary_t, `[${runPlayer.names.international}](${runPlayer.weblink}): [Video](${run.videos.links[0].uri})`);
+      embed.author = {
+        name: runPlayer.names.international,
+        url: runPlayer.weblink,
+        icon_url: `https://www.speedrun.com/images/flags/${runPlayer.location.country.code}.png` // eslint-disable-line camelcase
+      };
+      let assetIcon = actualGameData.assets[`trophy-${["1st", "2nd", "3rd", "4th"][run_.place-1]}`];
+      if(assetIcon) embed.thumbnail = {
+        url: assetIcon.uri
+      };
+      // resEmbed.addField(run.times.primary_t, `[${runPlayer.names.international}](${runPlayer.weblink}): [Video](${run.videos.links[0].uri})`);
+      resEmbeds.push(embed);
     });
-    data.msg.reply("", {embed: resEmbed});
+    data.msg.reply("", {embed: mainEmbed});
+    resEmbeds.forEach(embed => data.msg.reply("", {embed: embed}));
   }
 }));
 
