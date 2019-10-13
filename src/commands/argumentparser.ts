f514c146b30ab7a6466030c1b0ba90f7cd143cf4
pfg <omegariisthebest@gmail.com>
2019-10-12 21:07:31 -0700

    Move argument parsers to seperate functions

--- src/commands/argumentparser.ts
+++ src/commands/argumentparser.ts
@@ -25,7 +25,192 @@ function roleNameMatch(rolename: string, message: string) {
 	return rn === rm || `@${rn}` === rm;
 }
 
-//eslint-disable-next-line complexity
+export type ArgumentParserResult<T> = Promise<
+	{ result: "continue"; value: T; cmd: string } | { result: "exit" }
+>;
+
+export async function ChannelArgumentParser(
+	info: Info,
+	arg: ArgType,
+	cmd: string,
+	index: number
+): ArgumentParserResult<Discord.GuildChannel> {
+	if (!cmd.trim()) {
+		await info.error(
+			messages.arguments.channel_arg_not_provided(info, cmd, index)
+		);
+		return { result: "exit" };
+	}
+	if (!info.guild) {
+		await info.error(messages.failure.command_cannot_be_used_in_pms(info));
+		return { result: "exit" };
+	}
+	const match = cmd.match(/^[\S\s]*?([0-9]{14,})[^\s]*?\s*([\S\s]*)$/);
+	if (!match) {
+		await info.error(messages.arguments.no_channel(info, cmd, index));
+		return { result: "exit" };
+	}
+	const [, channelID, remainingCmd] = match;
+	const channel = info.guild.channels.get(channelID);
+	if (!channel) {
+		await info.error(
+			messages.arguments.channel_not_found(info, channelID, index)
+		);
+		return { result: "exit" };
+	}
+	return {
+		result: "continue",
+		value: channel,
+		cmd: remainingCmd
+	};
+}
+
+export async function EmojiArgumentParser(
+	info: Info,
+	arg: ArgType,
+	cmd: string,
+	index: number
+): ArgumentParserResult<Discord.GuildEmoji> {
+	if (!cmd.trim()) {
+		await info.error(
+			messages.arguments.emoji_arg_not_provided(info, cmd, index)
+		);
+		return { result: "exit" };
+	}
+	if (!info.guild) {
+		await info.error(messages.failure.command_cannot_be_used_in_pms(info));
+		return { result: "exit" };
+	}
+	const match = cmd.match(/^[\S\s]*?([0-9]{14,})[^\s]*?\s*([\S\s]*)$/);
+	if (!match) {
+		await info.error(messages.arguments.no_channel(info, cmd, index));
+		return { result: "exit" };
+	}
+	const [, emojiID, remainingCmd] = match;
+	const emoji = info.guild.emojis.get(emojiID);
+	if (!emoji) {
+		await info.error(
+			messages.arguments.emoji_not_found(info, emojiID, index)
+		);
+		return { result: "exit" };
+	}
+	return {
+		result: "continue",
+		value: emoji,
+		cmd: remainingCmd
+	};
+}
+
+export async function WordArgumentParser(
+	info: Info,
+	arg: ArgType,
+	cmd: string,
+	index: number
+): ArgumentParserResult<string> {
+	if (!cmd.trim()) {
+		await info.error(
+			messages.arguments.word_arg_not_provided(info, cmd, index)
+		);
+		return { result: "exit" };
+	}
+	const word = cmd.match(/^([\s]+)\s+([\S\s]+)/m);
+	if (!word) {
+		await info.error(messages.arguments.no_word(info, cmd, index));
+		return { result: "exit" };
+	}
+	const [, result, newCmd] = word;
+	return { result: "continue", value: result, cmd: newCmd };
+}
+
+export async function WordsArgumentParser(
+	info: Info,
+	arg: ArgType,
+	cmd: string,
+	index: number
+): ArgumentParserResult<string> {
+	if (!cmd.trim()) {
+		await info.error(
+			messages.arguments.words_arg_not_provided(info, cmd, index)
+		);
+		return { result: "exit" };
+	}
+	return { result: "continue", value: cmd.trim(), cmd: "" };
+}
+
+export async function RoleArgumentParser(
+	info: Info,
+	arg: ArgType,
+	cmd: string,
+	index: number
+): ArgumentParserResult<Discord.Role> {
+	if (!cmd.trim()) {
+		await info.error(
+			messages.arguments.role_arg_not_provided(info, cmd, index)
+		);
+		return { result: "exit" };
+	}
+	if (!info.guild) {
+		await info.error(messages.failure.command_cannot_be_used_in_pms(info));
+		return { result: "exit" };
+	}
+	const rolename = cmd.trim();
+	if (!rolename) {
+		await info.error(messages.arguments.no_role(info, cmd, index));
+		return { result: "exit" };
+	}
+	const roleID = (rolename.trim().match(/^[\S\s]*?([0-9]{16,})[\S\s]*$/) || [
+		"",
+		""
+	])[1];
+	let role: Discord.Role;
+	if (roleID) {
+		const foundRole = info.guild.roles.get(roleID);
+		if (!foundRole) {
+			await info.error(
+				messages.arguments.role_name_not_provided(info, roleID, index)
+			);
+			return { result: "exit" };
+		}
+		role = foundRole;
+	} else {
+		const matchingRoles = info.guild.roles
+			.array()
+			.filter(role => roleNameMatch(role.name, rolename));
+		if (matchingRoles.length > 1) {
+			await info.error(
+				messages.arguments.multiple_roles_found(
+					info,
+					rolename,
+					matchingRoles
+				)
+			);
+			return { result: "exit" };
+		}
+		if (matchingRoles.length === 0) {
+			await info.error(
+				messages.arguments.no_roles_found(info, rolename, index)
+			);
+			return { result: "exit" };
+		}
+		role = matchingRoles[0];
+	}
+	if (!role) {
+		await info.error(
+			messages.arguments.role_this_should_never_happen(
+				info,
+				rolename,
+				index
+			)
+		);
+		return { result: "exit" };
+	}
+	return {
+		result: "continue",
+		value: role,
+		cmd: ""
+	};
+}
+
 export async function OneArgumentParser(
 	info: Info,
 	arg: ArgType,
@@ -36,165 +221,19 @@ export async function OneArgumentParser(
 	| { result: "exit" }
 > {
 	if (arg === "channel") {
-		if (!cmd.trim()) {
-			await info.error(
-				messages.arguments.channel_arg_not_provided(info, cmd, index)
-			);
-			return { result: "exit" };
-		}
-		if (!info.guild) {
-			await info.error(
-				messages.failure.command_cannot_be_used_in_pms(info)
-			);
-			return { result: "exit" };
-		}
-		const match = cmd.match(/^[\S\s]*?([0-9]{14,})[^\s]*?\s*([\S\s]*)$/);
-		if (!match) {
-			await info.error(messages.arguments.no_channel(info, cmd, index));
-			return { result: "exit" };
-		}
-		const [, channelID, remainingCmd] = match;
-		const channel = info.guild.channels.get(channelID);
-		if (!channel) {
-			await info.error(
-				messages.arguments.channel_not_found(info, channelID, index)
-			);
-			return { result: "exit" };
-		}
-		return {
-			result: "continue",
-			value: channel,
-			cmd: remainingCmd
-		};
+		return ChannelArgumentParser(info, arg, cmd, index);
 	}
 	if (arg === "emoji") {
-		if (!cmd.trim()) {
-			await info.error(
-				messages.arguments.emoji_arg_not_provided(info, cmd, index)
-			);
-			return { result: "exit" };
-		}
-		if (!info.guild) {
-			await info.error(
-				messages.failure.command_cannot_be_used_in_pms(info)
-			);
-			return { result: "exit" };
-		}
-		const match = cmd.match(/^[\S\s]*?([0-9]{14,})[^\s]*?\s*([\S\s]*)$/);
-		if (!match) {
-			await info.error(messages.arguments.no_channel(info, cmd, index));
-			return { result: "exit" };
-		}
-		const [, emojiID, remainingCmd] = match;
-		const emoji = info.guild.emojis.get(emojiID);
-		if (!emoji) {
-			await info.error(
-				messages.arguments.emoji_not_found(info, emojiID, index)
-			);
-			return { result: "exit" };
-		}
-		return {
-			result: "continue",
-			value: emoji,
-			cmd: remainingCmd
-		};
+		return EmojiArgumentParser(info, arg, cmd, index);
 	}
 	if (arg === "word") {
-		if (!cmd.trim()) {
-			await info.error(
-				messages.arguments.word_arg_not_provided(info, cmd, index)
-			);
-			return { result: "exit" };
-		}
-		const word = cmd.match(/^([\s]+)\s+([\S\s]+)/m);
-		if (!word) {
-			await info.error(messages.arguments.no_word(info, cmd, index));
-			return { result: "exit" };
-		}
-		const [, result, newCmd] = word;
-		return { result: "continue", value: result, cmd: newCmd };
+		return WordArgumentParser(info, arg, cmd, index);
 	}
 	if (arg === "words...") {
-		if (!cmd.trim()) {
-			await info.error(
-				messages.arguments.words_arg_not_provided(info, cmd, index)
-			);
-			return { result: "exit" };
-		}
-		return { result: "continue", value: cmd.trim(), cmd: "" };
+		return WordsArgumentParser(info, arg, cmd, index);
 	}
 	if (arg === "role...") {
-		if (!cmd.trim()) {
-			await info.error(
-				messages.arguments.role_arg_not_provided(info, cmd, index)
-			);
-			return { result: "exit" };
-		}
-		if (!info.guild) {
-			await info.error(
-				messages.failure.command_cannot_be_used_in_pms(info)
-			);
-			return { result: "exit" };
-		}
-		const rolename = cmd.trim();
-		if (!rolename) {
-			await info.error(messages.arguments.no_role(info, cmd, index));
-			return { result: "exit" };
-		}
-		const roleID = (rolename
-			.trim()
-			.match(/^[\S\s]*?([0-9]{16,})[\S\s]*$/) || ["", ""])[1];
-		let role: Discord.Role;
-		if (roleID) {
-			const foundRole = info.guild.roles.get(roleID);
-			if (!foundRole) {
-				await info.error(
-					messages.arguments.role_name_not_provided(
-						info,
-						roleID,
-						index
-					)
-				);
-				return { result: "exit" };
-			}
-			role = foundRole;
-		} else {
-			const matchingRoles = info.guild.roles
-				.array()
-				.filter(role => roleNameMatch(role.name, rolename));
-			if (matchingRoles.length > 1) {
-				await info.error(
-					messages.arguments.multiple_roles_found(
-						info,
-						rolename,
-						matchingRoles
-					)
-				);
-				return { result: "exit" };
-			}
-			if (matchingRoles.length === 0) {
-				await info.error(
-					messages.arguments.no_roles_found(info, rolename, index)
-				);
-				return { result: "exit" };
-			}
-			role = matchingRoles[0];
-		}
-		if (!role) {
-			await info.error(
-				messages.arguments.role_this_should_never_happen(
-					info,
-					rolename,
-					index
-				)
-			);
-			return { result: "exit" };
-		}
-		return {
-			result: "continue",
-			value: role,
-			cmd: ""
-		};
+		return RoleArgumentParser(info, arg, cmd, index);
 	}
 	throw new Error(`Argument parser tried to parse ${arg}`);
 }
