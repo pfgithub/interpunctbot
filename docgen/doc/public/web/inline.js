85e2a3948c37cfd2922cca10679264d74424b3d9
pfg <omegariisthebest@gmail.com>
2019-11-17 13:01:25 -0800

    Mobile support

--- docgen/doc/public/web/inline.js
+++ docgen/doc/public/web/inline.js
@@ -1,8 +1,13 @@
 const linksToInline = Array.from(document.querySelectorAll("a[inline=true]"));
 linksToInline.forEach(async link => {
 	const content = await (await fetch(link.href)).text();
-	let contentMatch = content.match(/<!-- CONTENT STARTS HERE -->[\S\s]*<!-- CONTENT ENDS HERE -->/)[0];
-	contentMatch = contentMatch.replace(/<(\/?h)([1-6])>/g, (v, a, b) => `<${a}${+b+1}>`);
+	let contentMatch = content.match(
+		/<!-- CONTENT STARTS HERE -->[\S\s]*<!-- CONTENT ENDS HERE -->/
+	)[0];
+	contentMatch = contentMatch.replace(
+		/<(\/?h)([1-6])>/g,
+		(v, a, b) => `<${a}${+b + 1}>`
+	);
 	const bqc = document.createElement("div");
 	bqc.classList.add("blockquote-container");
 	const bqd = document.createElement("div");
@@ -13,3 +18,13 @@ linksToInline.forEach(async link => {
 	bqc.appendChild(bq);
 	link.parentNode.replaceChild(bqc, link); // unfortunately makes things pop in
 });
+
+const showSidebar = document.querySelector("button.showsidebar");
+showSidebar.addEventListener("click", () =>
+	document.querySelector(".sidebar").classList.add("shown")
+);
+
+const clickaway = document.querySelector(".clickaway");
+clickaway.addEventListener("click", () =>
+	document.querySelector(".sidebar").classList.remove("shown")
+);
