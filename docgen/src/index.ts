6c1075db6590753b3743717db20f094fa3c84cb6
pfg <omegariisthebest@gmail.com>
2019-11-18 14:14:19 -0800

    Add spacer at the bottom of scroll container

--- docgen/src/index.ts
+++ docgen/src/index.ts
@@ -256,6 +256,7 @@ function sidebar(
 			<div style="width: 100%; height: 84px; visibility: hidden;"></div>
 			<div style="height: 16px;"></div>
 			${raw(items.join(" "))}
+			<div style="height: 16px;"></div>
 		</div>
 	`;
 }
