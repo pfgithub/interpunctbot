55962a569c41288f70bbd519e03e276903f62a83
pfg <omegariisthebest@gmail.com>
2020-01-25 18:44:23 -0800

    new improved level format

--- src/commands/fun/goi.ts
+++ src/commands/fun/goi.ts
@@ -1,3 +1,6 @@
+import yaml from "js-yaml";
+import fs from "fs";
+import path from "path";
 import Router from "commandrouter";
 import { perr } from "../../..";
 import { messages } from "../../../messages";
@@ -6,168 +9,25 @@ import { createTimer } from "./checkers";
 
 const router = new Router<Info, Promise<any>>();
 
-const ge = {
-	skyg: "<:SkyGreen:650520232355692544>",
-	sand: "<:Sand:650519965950279684>",
-	normalcharacter: "<:normalpot:407696469722791937>",
-	goldpot: "<:goldpot:407697013493334036>", // once people have beat the game 50 times
-	bgtree: "🌲",
-	tree: "<:tree:413755772850143243>",
-	ocean: "<:Water:650520886897803264>",
-	left: "⬅️",
-	up: "⬆️",
-	right: "➡️",
-	rock: "<:Rock:650518888601354250>",
-	vwall: "<:VWall:650522702612004898>",
-	paddle: "<:Paddle:650523021609795616>",
-	prock: "<:PRock:650523309125009429>",
-};
-
 type GoDirectionSpec = (
 	| string
-	| { message: string; goto: string; rerollChance: number }
+	| { msg: string; goto?: string; reroll?: string }
 )[];
 
 type LevelSpec = {
-	text: { template: string[]; v: { [key: string]: string } };
-	go: { [emoji: string]: GoDirectionSpec };
+	text: string;
+	left: GoDirectionSpec;
+	up: GoDirectionSpec;
+	right: GoDirectionSpec;
 };
 
-const goilevels: { [key: string]: LevelSpec } = {
-	tree: {
-		text: {
-			template: ["ssss", "sgpt", "wfff"],
-			v: {
-				s: ge.skyg,
-				g: ge.bgtree,
-				p: "player",
-				t: ge.tree,
-				w: ge.ocean,
-				f: ge.sand,
-			},
-		},
-		go: {
-			[ge.left]: [
-				"*You pogo into the water and die. You are back at the beginning*",
-			],
-			[ge.up]: ["*You pogo up and fall back down*"],
-			[ge.right]: [
-				"*You try to reach the first branch of the tree but fall back down*",
-				"*You make it up to the first branch of the tree, but fall down*",
-				{
-					message: "*You pull yourself up over the tree*",
-					goto: "rock",
-					rerollChance: 0.25,
-				},
-				{
-					message:
-						"*You fling yourself over the tree with great force*",
-					goto: "basin",
-					rerollChance: 0.9,
-				},
-			],
-		},
-	},
-	rock: {
-		text: {
-			template: ["____", "|p#_", "fff_"],
-			v: {
-				_: ge.skyg,
-				"|": ge.tree,
-				p: "player",
-				"#": ge.rock,
-				f: ge.sand,
-			},
-		},
-		go: {
-			[ge.left]: [
-				{
-					message: "*You make your way back over the tree.*",
-					goto: "tree",
-					rerollChance: 0,
-				},
-			],
-			[ge.up]: ["*You pogo up and fall back down*"],
-			[ge.right]: [
-				{
-					message:
-						"*You climb over the rock and fall into the basin*",
-					goto: "basin",
-					rerollChance: 0,
-				},
-				{
-					message:
-						"*You fling yourself over the rock with great force*",
-					goto: "paddle",
-					rerollChance: 0.75,
-				},
-			],
-		},
-	},
-	basin: {
-		text: {
-			template: ["#__-w", "f__<w", "f_p_w", "ffffw"],
-			v: {
-				_: ge.skyg,
-				"|": ge.tree,
-				p: "player",
-				"#": ge.rock,
-				f: ge.sand,
-				"-": ge.paddle,
-				"<": ge.prock,
-				w: ge.vwall,
-			},
-		},
-		go: {
-			[ge.left]: [
-				"*You try to climb back out of the basin, but fail*",
-				"*You make it half way up the left side, but fall off*",
-				{
-					message: "You climb your way back out of the basin",
-					goto: "rock",
-					rerollChance: 0.99,
-				},
-			],
-			[ge.up]: [
-				"*You pogo up but miss the rock and paddle*",
-				"*You pogo up and grab the rock, but fall off*",
-				{
-					message:
-						"*You pogo and grab the rock and use it to fling yourself onto the paddle*",
-					goto: "paddle",
-					rerollChance: 0.75,
-				},
-			],
-			[ge.right]: ["*You run into the wall*"],
-		},
-	},
-	paddle: {
-		text: {
-			template: ["", "ap", ""],
-			v: {
-				a: "This level is not implemented yet!",
-				p: "player",
-			},
-		},
-		go: {
-			[ge.left]: [
-				{
-					message: "You fall back down to the basin",
-					rerollChance: 0,
-					goto: "basin",
-				},
-				{
-					message:
-						"You fling yourself off the paddle with great force",
-					rerollChance: 0.75,
-					goto: "rock",
-				},
-			],
-			[ge.up]: ["*you go up*"],
-			[ge.right]: ["*you go right*"],
-		},
-	},
-};
+const gamedata = yaml.safeLoad(
+	fs.readFileSync(path.join(__dirname, "goi.yaml"), "utf-8"),
+);
+
+const ge: { [key: string]: string | undefined } = gamedata.emojis;
+
+const goilevels: { [key: string]: LevelSpec } = gamedata.levels;
 
 router.add("goi", [], async (cmd: string, info) => {
 	if (info.db ? await info.db.getFunEnabled() : true) {
@@ -195,10 +55,10 @@ router.add("goi", [], async (cmd: string, info) => {
 
 	const gamemsg = await info.message.channel.send("Setting up game...");
 
-	let level = "tree";
-	await gamemsg.react(ge.left);
-	await gamemsg.react(ge.up);
-	await gamemsg.react(ge.right);
+	let level = gamedata.start;
+	await gamemsg.react(ge.left!);
+	await gamemsg.react(ge.up!);
+	await gamemsg.react(ge.right!);
 
 	const events: string[] = [];
 	let eventIndex = 1;
@@ -219,18 +79,11 @@ router.add("goi", [], async (cmd: string, info) => {
 		await gamemsg.edit(`**Getting Over It with Bennett Foddy**
 ${
 	goilevels[level]
-		? goilevels[level].text.template
-				.map(l =>
-					l
-						.split("")
-						.map(q =>
-							goilevels[level].text.v[q] === "player"
-								? ge.normalcharacter
-								: goilevels[level].text.v[q],
-						)
-						.join(""),
-				)
-				.join("\n")
+		? goilevels[level].text.replace(/{(.+?)}/g, (_, v) => {
+				v = v.trim();
+				if (v === "player") return ge.normalcharacter!;
+				return ge[v] || "{err`" + v + "`}";
+		  })
 		: `404! Level \`${level}\` not found!`
 }${events.map(ev => `\n${ev}`).join("")}`);
 		//eslint-disable-next-line require-atomic-updates
@@ -248,11 +101,16 @@ ${
 		if (typeof action === "string") {
 			return addEvent(action);
 		}
-		if (Math.random() < action.rerollChance && depth < 100) {
+		if (
+			Math.random() < parseInt(action.reroll || "0", 10) / 100 &&
+			depth < 100
+		) {
 			return doAction(actions, depth + 1);
 		}
-		addEvent(action.message);
-		level = action.goto;
+		addEvent(action.msg);
+		if (action.goto) {
+			level = action.goto;
+		}
 	};
 	const timer = createTimer([
 		60000,
@@ -270,10 +128,22 @@ ${
 		if (!goilevels[level]) {
 			return;
 		}
-		if (!goilevels[level].go[emov]) {
+		let action = undefined;
+		const levelt = goilevels[level];
+		if (emov === ge.left!) {
+			action = levelt.left;
+		} else if (emov === ge.up!) {
+			action = levelt.up;
+		} else if (emov === ge.right!) {
+			action = levelt.right;
+		} else {
 			return;
 		}
-		doAction(goilevels[level].go[emov]);
+		if (!action) {
+			console.log(level, action, emov);
+			throw new Error("!action for left/up/right");
+		}
+		doAction(action);
 		await updateMessage();
 	});
 	await updateMessage();
