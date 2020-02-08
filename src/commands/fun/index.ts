17a102017df084086a966ea9d1939fb80b2bc566
pfg <omegariisthebest@gmail.com>
2020-02-07 17:09:06 -0800

    v->g

--- src/commands/fun/index.ts
+++ src/commands/fun/index.ts
@@ -42,18 +42,18 @@ router.add("vdb", [], async (cmd: string, info) => {
 		return await info.error(messages.fun.fun_disabled(info));
 	}
 	await info.message.channel.send(`\`\`\`
-VNU vdb (VDB) 2.4.1
-Copyright (C) 2019 Vree Software Foundation, Inc.
-License VPLv3+: VNU VPL version 3 or later <http://vnu.org/licenses/vpl.html>
+GNU vdb (VDB) 2.4.1
+Copyright (C) 2019 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/Gpl.html>
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 Type "show copying" and "show warranty" for details.
 This VDB was configured as "x86_64-pc-btw-i-use-arch".
 Type "show configuration" for configuration details.
 For bug reporting instructions, please see:
-<http://www.vnu.org/software/vdb/bugs/>.
+<http://www.gnu.org/software/vdb/bugs/>.
 Find the VDB manual and other documentation resources online at:
-    <http://www.vnu.org/software/vdb/documentation/>.
+    <http://www.gnu.org/software/vdb/documentation/>.
 
 For help, type "help".
 Type "apropos word" to search for commands related to "word".
