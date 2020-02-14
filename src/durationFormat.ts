441deee89e6c96e5a1d1b4649f9c30dc19d98b29
pfg <omegariisthebest@gmail.com>
2020-02-13 23:49:49 -0800

    improve time formatting

--- src/durationFormat.ts
+++ src/durationFormat.ts
@@ -3,7 +3,18 @@ import moment from "moment";
 export function durationFormat(time: number) {
 	const duration = moment.duration(time, "ms");
 	// return duration.humanize();
-	return duration.format(
-		"y [years,] M [months,] w [weeks,] d [days,] h [hours,] m [minutes,] s [seconds,] SSS[ms]",
-	);
+	return duration
+		.format(
+			"y [years,] M [months,] w [weeks,] d [days,] h [hours,] m [minutes,] s [seconds,] SSS[ms]",
+		)
+		.split(", ")
+		.map(l =>
+			+l.trim().split(" ")[0] === 0
+				? undefined
+				: l === "000ms"
+				? undefined
+				: l,
+		)
+		.filter(q => q)
+		.join(", ");
 }
