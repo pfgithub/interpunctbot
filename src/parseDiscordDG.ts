d33bcb3d4eaa08be14868792f710faba45a66842
pfg <omegariisthebest@gmail.com>
2020-04-28 23:02:05 -0700

    √sorry

--- src/parseDiscordDG.ts
+++ src/parseDiscordDG.ts
@@ -164,7 +164,7 @@ const commands: {
 			/>
 			<div class="author bot">
 				inter·punct
-				<span class="bottag">BOT</span>
+				<span class="bottag"><svg aria-label="Verified Bot" class="botcheck" aria-hidden="false" width="16" height="16" viewBox="0 0 16 15.2"><path d="M7.4,11.17,4,8.62,5,7.26l2,1.53L10.64,4l1.36,1Z" fill="currentColor"></path></svg>BOT</span>
 			</div>
 			<div class="msgcontent">${args[0].safe}</div>
 		</div>`,
