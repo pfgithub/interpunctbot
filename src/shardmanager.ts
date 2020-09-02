9708601fe19455be6c41d2f83758bade7cdd4ce0
pfg <pfg@pfg.pw>
2020-09-02 13:12:12 -0700

    more shards

--- src/shardmanager.ts
+++ src/shardmanager.ts
@@ -9,7 +9,7 @@ if (!globalConfig.token)
 	);
 const manager = new ShardingManager("built/index.js", {
 	token: globalConfig.token,
-	totalShards: 1,
+	totalShards: 3,
 });
 
 manager.spawn().catch(e => console.log("spawn error", e));
