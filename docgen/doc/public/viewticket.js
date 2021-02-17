6285345a633e4e053c2effd91f3ae7098177620c
pfg <pfg@pfg.pw>
2021-02-16 19:51:11 -0800

    fix cors proxy

--- docgen/doc/public/viewticket.js
+++ docgen/doc/public/viewticket.js
@@ -1,6 +1,7 @@
 let urlParams = new URLSearchParams(window.location.search);
-fetch("https://cors-anywhere.herokuapp.com/"+encodeURI(urlParams.get("page"))).then((fr) => {
-	fr.text().then(txt => {
-		document.write(txt);
-	});
-})
+fetch("https://cors.bridged.cc/"+encodeURI(urlParams.get("page"))).then(fr => fr.text()).then(txt => {
+    document.write(txt);
+}).catch(e => {
+    console.log(e);
+    document.body.appendChild(document.createTextNode("Got error: "+e.toString()));
+});
