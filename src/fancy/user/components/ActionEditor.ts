92fa97f744893df20fd159f207c98f5022238ea3
pfg <pfg@pfg.pw>
2022-02-16 16:48:10 -0500

    note for the future

---
+++ src/fancy/user/components/ActionEditor.ts
@@ -0,0 +1,15 @@
+// props: {state, setState}
+
+export default function ActionEditor(): void {
+    // TODO
+    // This will let you edit actions
+    // Eg:
+    // - give role to runner
+    // - send message
+    // - send quote
+    // - …
+    // basically what the panel editor has
+    // but in a reusable component you can plug anywhere
+
+    // we can provide two: a list of buttons and a fullscreen editor probably
+}
