bd0c07815e71dd14af669f256d0f722d04948765
pfg <pfg@pfg.pw>
2022-10-23 09:46:06 -0400

    diagnose include guild id, shard id

--- src/fancy/user/commands/diagnose.ts
+++ src/fancy/user/commands/diagnose.ts
@@ -1,3 +1,4 @@
+import client from "../../../../bot";
 import Database, { CustomCommand } from "../../../Database";
 import { Message, renderEphemeral, renderError, SlashCommand, SlashCommandArgAutocompletableText, SlashCommandElement, SlashCommandInteractionResponse, u } from "../../fancylib";
 
@@ -17,13 +18,16 @@ export default function Command(): SlashCommandElement {
         if(event.interaction.guild_id == null) return renderError(u("guild null"));
         const guild_id = event.interaction.guild_id;
 
+        const guild_djs = client.guilds.cache.get(guild_id);
+		const shardv = guild_djs ? ` (shard id ${guild_djs.shardId})` : "";
+
         const db = new Database(guild_id);
 
         const modules: string[] = [];
 
         {
             modules.push(`Diagnostics:
-Guild ID: \`${guild_id}\``);
+Guild ID: \`${guild_id}\`${shardv}`);
         }
 
         const autodelete_info = await db.getAutodelete();
@@ -39,7 +43,7 @@ Use </ticket diagnose:791908656953950218> (/ticket diagnose) for diagnostics`);
         }
 
         modules.push(`Support server:
-If you need help with the bot, you can ask on the support server: https://discord.gg/HVWCeXc`);
+If you need help with the bot, you can ask on the support server: <https://discord.gg/HVWCeXc>`);
 
         return renderEphemeral(Message({
             text: u(modules.join("\n\n")),
