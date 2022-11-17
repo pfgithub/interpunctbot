6673522f59edbd336cb5c3065992292d21f37bc1
pfg <pfg@pfg.pw>
2022-11-17 14:15:34 -0500

    not actually sure if this one was necessary. and it still might be all broken because we might need this index to include the for_guild

--- migrations/20221117140250_index-fields-2.js
+++ migrations/20221117140250_index-fields-2.js
@@ -1,16 +1,4 @@
 
-// this query is slow
-// explain (analyze, buffers) select id from timed_events_at2 where completed = false and time < extract(epoch from current_timestamp) * 1000 order by time desc limit 1;
-// taking over 300ms!
-// - it index scans on idx_time, but it takes forever
-// so we need to include that in our index
-// - index on [time & completed]
-
-// const next_event = await ourk().where({
-//     'completed': false,
-// }).whereIn("for_guild", [...all_guilds]).orderBy('time', "asc").select("*").first();
-// yeah
-
 exports.up = async (knex, Promise) => {
 	await knex.schema.table("timed_events_at2", t => {
         t.index(["time", "completed"], "idx_time_and_completed", "btree"); // these fields are often used together
