ce5176810af9fb06b499bf7a96c763e052bd498f
pfg <pfg@pfg.pw>
2022-12-18 12:31:03 -0500

    notes

---
+++ src/fancy/user/actions/bookmark.ts
@@ -0,0 +1,9 @@
+/*
+
+*/
+async function bookmark(message: string, server: string): Promise<void> {
+    // dm a copy of the message to the user
+    // - cloned message content
+    // - links to attachments
+    // message link in a button
+}
