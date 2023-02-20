b33f51579c4aea5cdc8c80ba5b5be1650163f8ed
pfg <pfg@pfg.pw>
2023-02-20 22:12:37 +0000

    increase shard count

--- src/shardmanager.ts
+++ src/shardmanager.ts
@@ -9,7 +9,7 @@ if (globalConfig.token == null)
 	);
 const manager = new ShardingManager("built/index.js", {
 	token: globalConfig.token,
-	totalShards: 10,
+	totalShards: 20,
 });
 
 manager.spawn().catch(e => console.log("spawn error", e));
